47d10829dcb7041f13db91774bd265207c18ef43
Andy Boothe <andy.boothe@gmail.com>
2022-03-10 22:31:27 -0600

    add guava

---
+++ discourse-guava/src/main/java/com/sigpwned/discourse/guava/serialization/ByteSourceValueDeserializerFactory.java
@@ -0,0 +1,35 @@
+package com.sigpwned.discourse.guava.serialization;
+
+import java.io.File;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import com.google.common.io.ByteSource;
+import com.google.common.io.Files;
+import com.google.common.io.Resources;
+import com.sigpwned.discourse.core.ValueDeserializer;
+import com.sigpwned.discourse.core.ValueDeserializerFactory;
+
+/**
+ * Returns a ByteSource.
+ */
+public class ByteSourceValueDeserializerFactory implements ValueDeserializerFactory<ByteSource> {
+  @Override
+  public boolean isDeserializable(Type genericType, List<Annotation> annotations) {
+    return genericType.equals(ByteSource.class);
+  }
+
+  @Override
+  public ValueDeserializer<ByteSource> getDeserializer(Type genericType,
+      List<Annotation> annotations) {
+    return s -> {
+      try {
+        return Resources.asByteSource(new URL(s));
+      } catch (MalformedURLException e) {
+        return Files.asByteSource(new File(s));
+      }
+    };
+  }
+}
